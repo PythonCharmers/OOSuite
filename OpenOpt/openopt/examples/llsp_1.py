ab9f822776c425b7cba4b69ced325fd3deee9b62
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2009-08-25 07:51:08 +0000

    minor change for LLSP example
    git-svn-id: svn://openopt.org/PythonPackages@184 1214a21a-e15c-11dd-b329-07501fc18aa6

--- OpenOpt/openopt/examples/llsp_1.py
+++ OpenOpt/openopt/examples/llsp_1.py
@@ -12,16 +12,13 @@ for j in xrange(M):
     C[j] = 8*sin(4.0+arange(N)) + 15*cos(j)
 
 p = LLSP(C, d)
-#r = p.solve('lapack_dgelss') #requires scipy installed
-#p.debug=1
-#r = p.solve('toms587')
-r = p.solve('cvxopt_llsp')
+r = p.solve('lapack_dgelss') #requires scipy installed
 # or using single-precision:
 #r = p.solve('lapack_sgelss')
 
 #using llsp2nlp converter with an NLP solver:
 #r = p.solve('nlp:scipy_cg')
 
-print 'f_opt:', r.ff # 1601171.51977
+print 'f_opt:', r.ff # 611.600991401
 #print 'x_opt:', r.xf
 
