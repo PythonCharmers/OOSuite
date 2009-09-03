db3070bedcd942f43dbb8e8b091119d5106065d9
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2009-09-03 18:46:23 +0000

    minor change
    git-svn-id: svn://openopt.org/PythonPackages@216 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/nlp1.py
+++ FuncDesigner/FuncDesigner/examples/nlp1.py
@@ -1,5 +1,6 @@
-from FuncDesigner import *
 from openopt import NLP
+from FuncDesigner import *
+
 
 a, b, c = oovars('a', 'b', 'c')
 f = sum(a*[1, 2])**2+b**2+c**2
@@ -7,11 +8,11 @@ startPoint = {a:[100, 12], b:2, c:40} # however, you'd better use numpy arrays i
 p = NLP(f, startPoint)
 p.constraints = [(2*c+a-10)**2<1.5 + 0.1*b, (a-10)**2<1.5, a[0]>8.9, a+b>[ 7.97999836, 7.8552538 ], \
 a<9, (c-2)**2<1, b<-1.02, c>1.01, ((b+2*c-1)**2).eq(0)]
-r = p.solve('ralg', plot=True)
+r = p.solve('ralg', plot=False)
 print r.xf
 
 """ Expected output:
 ...
-objFunValue: 717.75631 (feasible, max constraint =  7.44605e-07)
-{a: array([ 8.99999792,  8.87525277]), b: array([-1.01999971]), c: array([ 1.0613562])}
+objFunValue: 717.65008 (feasible, max constraint =  5.57207e-07)
+{a: array([ 9.00000015,  8.87525291]), b: array([-1.01999967]), c: array([ 1.01000454])}
 """
