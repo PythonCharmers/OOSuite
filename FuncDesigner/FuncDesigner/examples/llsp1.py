afa6829cc883db2ab61330a2899e233907aa3db3
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2009-09-25 10:15:14 +0000

    minor doc update
    git-svn-id: svn://openopt.org/PythonPackages@257 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/llsp1.py
+++ FuncDesigner/FuncDesigner/examples/llsp1.py
@@ -3,9 +3,12 @@ from openopt import LLSP
 
 # create some variables
 a, b, c = oovars('a', 'b', 'c')
+# or just a, b, c = oovars(3)
 
-# start point is required to set dimensions of variables
-startPoint = {a:0, b:0, c:0} # also, it is used for some converters e.g. r = p.solve('nlp:ralg')
+# start point is unused by lapack_dgelss and lapack_dgelss
+# but it is required to set dimensions of variables
+# also, it is used for some converters e.g. r = p.solve('nlp:ralg')
+startPoint = {a:0, b:0, c:0} 
 
 # overdetermined system of 4 linear equations with 3 variables
 f = [2*a+3*b-4*c+5, 2*a+13*b+15, a+4*b+4*c-25, 20*a+30*b-4*c+50]
