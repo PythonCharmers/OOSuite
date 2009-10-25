c05872c73a85a96b34ef436592f45fa76a6f6428
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2009-10-25 14:21:23 +0000

    minor changes for FD sle example
    git-svn-id: svn://openopt.org/PythonPackages@264 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/sle2.py
+++ FuncDesigner/FuncDesigner/examples/sle2.py
@@ -8,20 +8,21 @@ n = 100
 a, b, c = oovar(), oovar(size=n), oovar(size=2*n)
 
 # let's construct some linear functions R^i -> R^j
-f1 = a + b.sum() # R^(n+1) -> R
+# in Python range(m, k) is [m, m+1, ..., m+k-1]
+f1 = a + sum(b*range(5, n+5)) # R^(n+1) -> R
 f2 = a + 2*b + c.sum() # R^(2n+1) -> R^(n)
 
 # you could use size of oovars
 f3 = a + a.size + 2*c.size # R^(2n+1) -> R; a.size and c.size will be resolved into 1 and 2*n
 
-f4 = c + f1 + 0.5*f2.sum() + 4*f3
+f4 = c + range(4, 2*n+4) + f1 + 0.5*f2.sum() + 4*f3
 
 # We can use "for" cycle:
 for i in xrange(4):
     f4 = 0.5*f4 + a + f1 + 1
 
 # Python list of linear equations 
-f = [a+f4+5, 2*a+b+15, a+4*b.sum()+2*c.sum()-45]
+f = [a+f4+5, 2*a+b*range(10, n+10)+15, a+4*b.sum()+2*c.sum()-45]
 # alternatively, you could pass equations:
 #f = [(a+f4).eq(-5), (2*a+b).eq(-15), a.eq(-4*b.sum()-2*c.sum()+45)]
 
