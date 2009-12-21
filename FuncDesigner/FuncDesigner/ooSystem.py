48277c51c5b89483e76c890fa6a93e4e3cc2fc87
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2009-12-21 09:25:50 +0000

    update for ooSystem constructor
    git-svn-id: svn://openopt.org/PythonPackages@354 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/ooSystem.py
+++ FuncDesigner/FuncDesigner/ooSystem.py
@@ -1,20 +1,26 @@
-
+from misc import FuncDesignerException
+from ooFun import oofun
 
 class ooSystem(set):
+    def __init__(self, *args,  **kwargs):
+        assert len(kwargs) == 0, 'ooSystem constructor has no implemented kwargs yet'
+        for arg in args:
+            if isinstance(arg, set):
+               self.update(arg)
+            elif isinstance(arg, list) or isinstance(arg, tuple):
+                self.update(set(arg))
+            elif isinstance(arg, oofun):
+                self.add(arg)
+            else:
+                raise FuncDesignerException('Incorrect type %s in ooSystem constructor' % type(arg))
+        
     __call__ = lambda self, point: ooSystemState([(elem, elem(point)) for elem in self])
         
 class ooSystemState(dict):
-    # TODO: add .pp (prettyprint)
-    
+   
     def __repr__(self):
-        r = []
-        for key, val in self.items():
-            r.append(str(key)+'='+str(val)+'\n')
+        r = [str(key)+'='+str(val)+'\n' for key, val in self.items()]
         return ''.join(r)[:-1]
-
-
-#def EmptyFunc(*args, **kwargs):
-#    print ('warning - while invoking ooSystemState.pp (pretty print of ooSystemState instance) you could omit brackets')
 
 
 
