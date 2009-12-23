35006908b5eb9bcf44f93ff8d347c175bf72a901
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2009-12-23 10:37:22 +0000

    add __getitem__ method to ooSystem
    git-svn-id: svn://openopt.org/PythonPackages@355 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/ooSystem.py
+++ FuncDesigner/FuncDesigner/ooSystem.py
@@ -1,6 +1,7 @@
 from misc import FuncDesignerException
 from ooFun import oofun
 
+
 class ooSystem(set):
     def __init__(self, *args,  **kwargs):
         assert len(kwargs) == 0, 'ooSystem constructor has no implemented kwargs yet'
@@ -13,14 +14,28 @@ class ooSystem(set):
                 self.add(arg)
             else:
                 raise FuncDesignerException('Incorrect type %s in ooSystem constructor' % type(arg))
+    
+    # [] yields result wrt allowed contol:
+    __getitem__ = lambda self, point: ooSystemState([(elem, elem(point)) for elem in self])
+    
+    # () yields exact result (ignoring contol):
+    def __call__(self, point):
+        assert isinstance(point,  dict), 'argument should be Python dictionary'
+        tmp = self.__getitem__(point)
+        r = []
+        for key, val in tmp.items():
+            r.append((key, key(point, contol=0.0))) if key.isConstraint else r.append((key, val))
+        return ooSystemState(r)
         
-    __call__ = lambda self, point: ooSystemState([(elem, elem(point)) for elem in self])
         
+        
+####################### ooSystemState #########################
 class ooSystemState(dict):
-   
-    def __repr__(self):
-        r = [str(key)+'='+str(val)+'\n' for key, val in self.items()]
-        return ''.join(r)[:-1]
+    __repr__ = lambda self: ''.join(['\n'+str(key)+'='+str(val) for key, val in self.items()])[1:]
+    def __call__(self, *args,  **kwargs):
+        assert len(kwargs) == 0, "ooSystemState method '__call__' has no implemented kwargs yet"
+        # TODO: implement it
+        # access by elements and thier names
 
 
 
