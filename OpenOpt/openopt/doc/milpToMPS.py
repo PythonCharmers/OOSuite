87935c74a7ad1e4a70b77825136abdf7f984edd3
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-01-31 09:13:20 +0000

    add examples of export LP/MILP to MPS format files
    git-svn-id: svn://openopt.org/PythonPackages@406 1214a21a-e15c-11dd-b329-07501fc18aa6

---
+++ OpenOpt/openopt/doc/milpToMPS.py
@@ -0,0 +1,34 @@
+# Example of export OpenOpt MILP to MPS file
+# you should have lpsolve and its Python binding properly installed
+# (you may take a look at the instructions from openopt.org/LP)
+
+from numpy import *
+from openopt import MILP
+
+f = [1, 2, 3, 4, 5, 4, 2, 1]
+
+# indexing starts from ZERO!
+# while in native lpsolve-python wrapper from 1
+# so if you used [5,8] for native lp_solve python binding
+# you should use [4,7] instead
+intVars = [4, 7]
+
+lb = -1.5 * ones(8)
+ub = 15 * ones(8)
+A = zeros((5, 8))
+b = zeros(5)
+for i in xrange(5):
+    for j in xrange(8):
+        A[i,j] = -8+sin(8*i) + cos(15*j)
+    b[i] = -150 + 80*sin(80*i)
+
+p = MILP(f=f, lb=lb, ub=ub, A=A, b=b, intVars=intVars)
+
+# if file name not ends with '.MPS' or '.mps'
+# then '.mps' will be appended
+r = p.exportToMPS('/home/dmitrey/PyTest/milp')
+# or write into current dir: 
+# r = p.exportToMPS('milp')
+
+# f_opt is 25.801450769161505
+# x_opt is [ 15. 10.15072538 -1.5 -1.5 -1.  -1.5 -1.5 15.]
