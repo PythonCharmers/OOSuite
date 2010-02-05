7936a7c398424260b768c52f3ed8f96680902276
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-02-05 16:08:20 +0000

    updated LLSP example
    git-svn-id: svn://openopt.org/PythonPackages@423 1214a21a-e15c-11dd-b329-07501fc18aa6

--- OpenOpt/openopt/examples/llsp_1.py
+++ OpenOpt/openopt/examples/llsp_1.py
@@ -3,22 +3,24 @@ __docformat__ = "restructuredtext en"
 from numpy import empty, sin, cos, arange
 from openopt import LLSP
 
-M, N = 3, 2
+M, N = 1500, 1000
 C = empty((M,N))
 d =  empty(M)
 
 for j in xrange(M):
     d[j] = 1.5*N+80*sin(j)
     C[j] = 8*sin(4.0+arange(N)) + 15*cos(j)
+    
+""" alternatvely, try the sparse problem - lsqr solver can take benefits of it:
+for j in xrange(M):
+    d[j] = 1.5*N+80*sin(j)
+    C[j, j%N] = 15*cos(j) #+ 8*sin(4.0+arange(N))
+    C[j, (1 + j)%N] = 15*cos(j) #+ 8*sin(4.0+arange(N))
+"""
 
 p = LLSP(C, d)
-r = p.solve('lapack_dgelss') #requires scipy installed
-# or using single-precision:
-#r = p.solve('lapack_sgelss')
+r = p.solve('lsqr')
 
-#using llsp2nlp converter with an NLP solver:
-#r = p.solve('nlp:scipy_cg')
-
-print 'f_opt:', r.ff # 611.600991401
+print 'f_opt:', r.ff # 2398301.68347
 #print 'x_opt:', r.xf
 
