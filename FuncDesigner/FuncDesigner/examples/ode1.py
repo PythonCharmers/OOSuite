5b18b87f12b9e9b428df996b643b1bb6704418b2
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-05-15 17:35:22 +0000

    minor doc fix
    git-svn-id: svn://openopt.org/PythonPackages@545 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/ode1.py
+++ FuncDesigner/FuncDesigner/examples/ode1.py
@@ -16,7 +16,7 @@ equations = {
 
 startPoint = {a: 3, b: 4, c: 5}
 
-timeArray = arange(0, 1, 0.01) # 0, 0.01, 0.02, 0.03, ..., 1
+timeArray = arange(0, 1, 0.01) # 0, 0.01, 0.02, 0.03, ..., 0.99
 
 # assign ODE. 3rd argument (here "t") is time variable that is involved in differentiation.
 myODE = ode(equations, startPoint, t, timeArray)
