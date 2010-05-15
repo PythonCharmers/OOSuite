f3694a68eb34b1cfade622ec6679f09949d66f8d
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-05-15 20:01:19 +0000

    2nd example for FD ODE
    git-svn-id: svn://openopt.org/PythonPackages@550 1214a21a-e15c-11dd-b329-07501fc18aa6

---
+++ FuncDesigner/FuncDesigner/examples/ode2.py
@@ -0,0 +1,34 @@
+""" Advanced FuncDesigner example for solving ODE with automatic differentiation """
+
+from FuncDesigner import *
+from numpy import arange, zeros,  ones
+
+N = 50
+
+# create some variables
+x, t = oovars('x', 't')
+y = oovar('y', size = N)
+z = oovar('z', size = 2*N)
+
+# Python dict of ODEs: da/dt, db/dt, dc/dt
+equations = {
+             x: 2*x + exp(5-2*t), 
+             y: arcsin(t/5) + 2*cos(y),  #+sum(z), # sum(z) and z.sum() are same, as well as for numpy arrays
+             z: cos(z/10) + sin(x) + 4*cos(y.sum()) - sinh(2*t) 
+             }
+
+startPoint = {x: 3, y: 4*ones(N), z: 5*zeros(2*N)}
+
+timeArray = arange(0, 1, 0.01) # 0, 0.01, 0.02, 0.03, ..., 0.99
+
+# assign ODE. 3rd argument (here "t") is time variable that is involved in differentiation.
+myODE = ode(equations, startPoint, t, timeArray)
+
+# Probably output will be changed till next release to more OpenOpt-like.
+# Currently it's exact scipy.integrate.odeint output
+r = myODE.solve()
+X,  Y,  Z = r(x, y, z)
+print(r.msg) # r.extras.infodict contains whole scipy.integrate.odeint infodict
+"""
+Integration successful.
+"""
