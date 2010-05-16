62e64ea8b5f6d6ab55f4a914d7e9305cddeb5902
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-05-16 07:22:43 +0000

    minor doc update
    git-svn-id: svn://openopt.org/PythonPackages@553 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/ode2.py
+++ FuncDesigner/FuncDesigner/examples/ode2.py
@@ -13,8 +13,8 @@ z = oovar('z', size = 2*N)
 # Python dict of ODEs: da/dt, db/dt, dc/dt
 equations = {
              x: 2*x + exp(5-2*t), 
-             y: arcsin(t/5) + 2*cos(y),  #+sum(z), # sum(z) and z.sum() are same, as well as for numpy arrays
-             z: cos(z/10) + sin(x) + 4*cos(y.sum()) - sinh(2*t) 
+             y: arcsin(t/5) + 2*cos(y) + cos(sum(z)), # sum(z) and z.sum() are same, as well as for numpy arrays
+             z: cos(z/10) + sin(x) + 4*cos(y.sum()) - 0.001*sinh(2*t) 
              }
 
 startPoint = {x: 3, y: 4*ones(N), z: 5*zeros(2*N)}
@@ -27,8 +27,5 @@ myODE = ode(equations, startPoint, t, timeArray)
 # Probably output will be changed till next release to more OpenOpt-like.
 # Currently it's exact scipy.integrate.odeint output
 r = myODE.solve()
-X,  Y,  Z = r(x, y, z)
+X,  Y,  Z = r(x, y, z) # X.size = 100, Y.shape = (50, 100), Z.shape = (100, 100)
 print(r.msg) # r.extras.infodict contains whole scipy.integrate.odeint infodict
-"""
-Integration successful.
-"""
