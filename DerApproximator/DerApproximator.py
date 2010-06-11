100a9334b3c2fe42a90e2f3d13e7a9389305b830
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-06-11 09:22:18 +0000

    add get_d2() for DerApproximator and a new stencil
    git-svn-id: svn://openopt.org/PythonPackages@563 1214a21a-e15c-11dd-b329-07501fc18aa6

--- DerApproximator/DerApproximator.py
+++ DerApproximator/DerApproximator.py
@@ -1,7 +1,7 @@
 """finite-difference derivatives approximation"""
 
 from numpy import atleast_1d, atleast_2d, isfinite, ndarray, nan, empty, where, ndarray, log10, hstack, floor, ceil, \
-argmax, asscalar, abs, isscalar, asfarray, asarray, isnan
+argmax, asscalar, abs, isscalar, asfarray, asarray, isnan, array
 
 class DerApproximatorException:
     def __init__(self,  msg):
@@ -11,18 +11,17 @@ class DerApproximatorException:
 
 def get_d1(fun, vars, diffInt=1.5e-8, pointVal = None, args=(), stencil = 2, varForDifferentiation = None, exactShape = False):
     """
-    Usage: get_d1(fun, x, diffInt=1.5e-8, pointVal = None, args=(), stencil = 2, varForDifferentiation = None)
+    Usage: get_d1(fun, x, diffInt=1.5e-8, pointVal = None, args=(), stencil = 2, varForDifferentiation = None, exactShape = False)
     fun: R^n -> R^m, x0 from R^n: function and point where derivatives should be obtained 
     diffInt - step for stencil
     pointVal - fun(x) if known (it is used from OpenOpt and FuncDesigner)
-    args - additional args for fun, if not () fun(x, *args) will be involved 
+    args - additional args for fun, if not equalk to () then fun(x, *args) will be involved 
     stencil = 1: (f(x+diffInt) - f(x)) / diffInt
     stencil = 2: (f(x+diffInt) - f(x-diffInt)) / (2*diffInt)
     varForDifferentiation - the parameter is used from FuncDesigner
     exactShape - set True to forbid possible flattering for 1D arrays
     """
     assert type(vars) in [tuple,  list,  ndarray, float, dict]
-    
     #assert asarray(diffInt).size == 1,  'vector diffInt are not implemented for oofuns yet'      
     diffInt = atleast_1d(diffInt)
     if atleast_1d(diffInt).size > 1: assert type(vars) == ndarray, 'not implemented yet'
@@ -47,7 +46,7 @@ def get_d1(fun, vars, diffInt=1.5e-8, pointVal = None, args=(), stencil = 2, var
         raise DerApproximatorException('Currently DerApproximatorx cannot handle functions with (ndim of output) > 1 , only vectors are allowed')
     M = v_0.size
     r = []
-
+    
     for i in xrange(len(Vars)):
         if varForDifferentiation is not None and i != varForDifferentiation: continue
         if not isscalar(Args[i]):
@@ -74,25 +73,38 @@ def get_d1(fun, vars, diffInt=1.5e-8, pointVal = None, args=(), stencil = 2, var
             di = float(asscalar(diff_int[j]))
             tmp = S[j]
             di = diff_int[j]
+            di2 = di / 2.0
             S[j] += di
             TMP = fun(*Args)
-            if not isscalar(TMP): TMP = hstack(TMP)
+            #if not isscalar(TMP): TMP = hstack(TMP)
             v_right = atleast_1d(TMP)# TODO: fix it for matrices with ndims>1
             S[j] = tmp 
             # not Args[i][j] -= di, because it can change primeval Args[i][j] value 
             # and check for key == same value will be failed
             
-            has_nonfinite = not all(isfinite(v_right))
-            if stencil == 2 or has_nonfinite:
+            has_nonfinite = not all(isfinite(v_right.flatten()))
+            if stencil >= 2 or has_nonfinite:
                 S[j] -= di
                 v_left = atleast_1d(fun(*Args))
                 S[j] = tmp
                 if has_nonfinite:
                     d1[:, agregate_counter] = (v_0-v_left) / di
-                else:
+                elif stencil == 2:
                     d1[:, agregate_counter] = (v_right-v_left) / (2.0 * di)
+                else:
+                    assert stencil == 3
+                    S[j] -= di2
+                    v_subleft = atleast_1d(fun(*Args))
+                    S[j] = tmp
+                    
+                    S[j] += di2
+                    v_subright = atleast_1d(fun(*Args))
+                    S[j] = tmp
+                    
+                    d1[:, agregate_counter] = (v_left - v_right + 8.0 * (v_subright-v_subleft)) / (6.0 * di)
             else:
                 d1[:, agregate_counter] = (v_right-v_0) / di
+                
             agregate_counter += 1 # TODO: probably other values for n-dim arrays
             
         # TODO: fix it for arrays with ndim > 2
@@ -110,7 +122,7 @@ def check_d1(fun, fun_d, vars, func_name='func', diffInt=1.5e-8, pointVal = None
     fun_d - user-provided routine for derivatives evaluation to be checked 
     diffInt - step for stencil
     pointVal - fun(x) if known (it is used from OpenOpt and FuncDesigner)
-    args - additional args for fun, if not () fun(x, *args) will be involved 
+    args - additional args for fun, if not equalk to () then fun(x, *args) will be involved 
     stencil = 1: (f(x+diffInt) - f(x)) / diffInt
     stencil = 2: (f(x+diffInt) - f(x-diffInt)) / (2*diffInt)
     maxViolation - threshold for reporting of incorrect derivatives
@@ -194,86 +206,37 @@ def check_d1(fun, fun_d, vars, func_name='func', diffInt=1.5e-8, pointVal = None
         print('derivatives are equal')
     print(75 * '*')
     
-#def check_d1(fun, fun_d, vars, func_name='func', diffInt=1e-7, pointVal = None, args=(), twoSized = True, maxViolation=0.01):
-#    info_numerical = get_d1(fun, vars, diffInt=diffInt, pointVal = pointVal, args=args, twoSized = twoSized)
-#    
-#    if type(vars) not in [list, tuple]:
-#        Vars = [vars, ]
-#    else: Vars = list(vars)
-#    
-#    if type(args) != tuple:
-#        args = (args, )
-#    Args = list(tuple(Vars) + args)
-#    
-#    #info_user = fun_d(*Args)
-#    info_user = fun_d()
-#    
-#    # TODO: check for fixed oofuncs
-#    
-#    if info_numerical.shape != info_user.shape:
-#        raise DerApproximatorException('user-supplied gradient for ' + func_name + ' has other size than the one, obtained numerically: '+ \
-#        str(info_numerical.shape) + ' expected, ' + str(info_user.shape) + ' obtained')
-#    
-#    Diff = 1 - (info_user+1e-8)/(info_numerical + 1e-8) # 1e-8 to suppress zeros
-#    log10_RD = log10(abs(Diff)/maxViolation+1e-150)
-#
-#    #TODO: omit flattering
-#    d = hstack((info_user.reshape(-1,1), info_numerical.reshape(-1,1), Diff.reshape(-1,1)))
-#    
-#    print('Checking user-supplied gradient for ' + func_name + ' of shape ' + str(info_user.shape))
-#    print('according to:')
-#    print('    diffInt = ' + str(diffInt)) # TODO: ADD other parameters: allowed epsilon, maxDiffLines etc
-#    print('    |1 - info_user/info_numerical| < maxViolation = '+ str(maxViolation))
-#
-#    if info_numerical.ndim > 1: useDoubleColumn = True
-#    else: useDoubleColumn = False
-#
-#    cond_same = all(abs(Diff).flatten() <= maxViolation)
-#    if not cond_same:
-#        ss = '    '
-#            
-#        if useDoubleColumn:
-#            ss = ' i,j: d' + func_name + '[i]/dx[j]'
-#
-#        s = func_name + ' num  ' + ss + '   user-supplied     numerical               RD'
-#        print(s)
-#
-#    ns = ceil(log10(d.shape[0]))
-#    counter = 0
-#    fl_info_user = info_user.flatten()
-#    fl_info_numerical = info_numerical.flatten()
-#    if len(Diff.shape) == 1:
-#        Diff = Diff.reshape(-1,1)
-#        log10_RD = log10_RD.reshape(-1,1)
-#    for i in xrange(Diff.shape[0]):
-#        for j in xrange(Diff.shape[1]):
-#            if abs(Diff[i,j]) < maxViolation: continue
-#            counter += 1
-#            k = Diff.shape[1]*i+j
-#            nSpaces = ns - floor(log10(k+1))+2
-#            if useDoubleColumn:  ss = str(i) + ' / ' + str(j)
-#            else: ss = ''
-#
-#            if len(Diff.shape) == 1 or Diff.shape[1] == 1: n2 = 0
-#            else: n2 = 15
-#            s = '    ' + ('%d' % k).ljust(5) + ss.rjust(n2) + ('%+0.3e' % fl_info_user[k]).rjust(19) + ('%+0.3e' % fl_info_numerical[k]).rjust(15) + ('%d' % int(ceil(log10_RD[i,j]))).rjust(15)
-#            print(s)
-#
-#    diff_d = abs(d[:,0]-d[:,1])
-#    ind_max = argmax(diff_d)
-#    val_max = diff_d[ind_max]
-#    if not cond_same:
-#        print('max(abs(d_user - d_numerical)) = ' + str(val_max))
-#        print('(is registered in func number ' + str(ind_max) + ')')
-#    else:
-#        print('derivatives are equal')
-#
-#    print(64 * '*')
-#    
-#
-#    
-#    
-#    
-#    
-#    
-#    
+    
+def get_d2(fun, vars, fun_d = None, diffInt = 1.5e-8, pointVal = None, args=(), stencil = 3, varForDifferentiation = None, exactShape = True, pointD1 = None):
+    """
+    Usage: get_d2(fun, x, fun_d = None, diffInt = 1.5e-8, pointVal = None, args=(), stencil = 3, varForDifferentiation = None, exactShape = True)
+    
+    fun: R^n -> R^m, x0 from R^n: function and point where derivatives should be obtained 
+    currently implemented for m=1 only!
+    
+    diffInt - step for stencil
+    pointVal - fun(x) if known (it is used from OpenOpt and FuncDesigner)
+    args - additional args for fun, if not equalk to () then fun(x, *args) will be involved 
+    stencil - parameter for lower-level get_d1, default 2 (see get_d1 documentation)
+    varForDifferentiation - the parameter is used from FuncDesigner
+    exactShape - set True to forbid possible flattering for 1D arrays
+    """
+    
+    # TODO: reduce number of func evals via (j,i) -> (i,j)
+    
+    if fun_d is None: 
+        fun_d = lambda x: get_d1(fun, x, diffInt=diffInt, pointVal = pointVal, args=args, stencil = stencil, \
+                                 varForDifferentiation = varForDifferentiation, exactShape = exactShape)
+                                 
+    # TODO: get rid of it when m could be greater than 1
+#    if pointVal is None:
+#        pointVal = fun(vars)
+
+    # TODO: involve start value of 1st derivative,  if known
+    point_D1_Val = (fun_d(vars) if pointD1 is None else array(pointD1, float)).flatten()
+    
+
+    return get_d1(fun_d, vars, diffInt=diffInt, pointVal = point_D1_Val, args=args, stencil = stencil, \
+                  varForDifferentiation = varForDifferentiation, exactShape = exactShape)
+    
+
