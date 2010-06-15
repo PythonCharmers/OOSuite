d390d8746bb65da87b71357591be622d8b4ae3b5
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-06-15 14:40:43 +0000

    sle changes
    git-svn-id: svn://openopt.org/PythonPackages@585 1214a21a-e15c-11dd-b329-07501fc18aa6

--- OpenOpt/openopt/solvers/Standalone/defaultSLEsolver_oo.py
+++ OpenOpt/openopt/solvers/Standalone/defaultSLEsolver_oo.py
@@ -32,17 +32,22 @@ class defaultSLEsolver(baseSolver):
         elif solver in self.sparseSolvers:
             useDense = False
         elif solver == 'autoselect':
-            if not hasattr(scipy.sparse, 'linalg') or (isinstance(p.C, ndarray) and len(p.C.nonzero()[0]) > 0.25 * p.C.size) or not scipyInstalled:
-                if not hasattr(scipy.sparse, 'linalg'):
-                    s = """you use new version of scipy where scipy.sparse.linalg was moved to scikits.umfpack. 
-                    It is not ajusted with the version of our soft you are using yet. 
-                    Thus SLE will be solved as dense. 
-                    If sparsity is strongly required, you could use rendering (see FuncDesigner doc)"""
-                    p.pWarn(s)
+            if not scipyInstalled or not hasattr(scipy.sparse, 'linalg'):
+                useDense = True
+                if isinstance(p.C, ndarray) and len(p.C.nonzero()[0]) > 0.25 * p.C.size > 10000: 
+                    if not hasattr(scipy.sparse, 'linalg'):
+                        s = """you use new version of scipy where scipy.sparse.linalg was moved to scikits.umfpack. 
+                        It is not ajusted with the version of our soft you are using yet. 
+                        Thus SLE will be solved as dense. 
+                        If sparsity is strongly required, you could use rendering (see FuncDesigner doc)"""
+                        p.pWarn(s)
+                    else:
+                        pass
+                    #p.pWarn(scipyAbsentMsg)
                     
                 solver = self.defaultDenseSolver
                 self.matrixSLEsolver = solver
-                useDense = True
+                
             else:
                 solver = self.defaultSparseSolver
                 self.matrixSLEsolver = solver
