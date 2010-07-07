ca699a0edcd925d43a48932a4d9757255ca465f5
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-07-07 07:48:57 +0000

    some changes for FD integrator example
    git-svn-id: svn://openopt.org/PythonPackages@640 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/integrate1.py
+++ FuncDesigner/FuncDesigner/examples/integrate1.py
@@ -1,3 +1,8 @@
+"""
+Example of FuncDesigner functions integration
+Currently scipy.intergate.quad is used Fortan library QUADPACK, (R^n->R^1 only),
+maybe in future higher order solvers will be connected (
+"""
 from FuncDesigner import *
 a, b, c = oovars('a', 'b', 'c') 
 f1,  f2 = sin(a)+cosh(b), 2*b+3*c.sum()
@@ -6,18 +11,24 @@ f3 = 2*a*b*prod(c) + f1*cos(f2)
 point1 = {a:1, b:2, c:[3, 4, 5]}
 point2 = {a: -10.4, b: 2.5,  c:[3.2, 4.8]}
 
-# usage:
-# myOOFun = integrate.scipy_quad(integration_oofun, lower_bound,  upper_bound, integration_oovar,  /{optionally: parameters for  scipy.integrate.quad}/) 
+# Usage:
+# myOOFun = integrator(integration_oofun, domain) 
+# where domain is tuple (integration_oovar, lower_bound,  upper_bound)
 
-f4 = integrate.scipy_quad(f1+2*f2+3*f3, -1, 1, b) 
+domain = (b, -1, 1)
+f4 = integrator(f1+2*f2+3*f3, domain)
 print f4(point1), f4(point2) # Expected output: 147.383792876, 102.143425528
 
-# integral bounds can be oofuns as well:
-f5 = integrate.scipy_quad(f1+2*f2+3*f3, 10.4, f2+5*sin(f1), a) 
-f6 = integrate.scipy_quad(f1+2*f2+3*f3, cos(f2+2*f3), -9.3, a) 
-f7 = integrate.scipy_quad(f1+2*f2+3*sqrt(abs(f3)), cos(f2+2*f3), f2+5*sin(f1), a) 
+#                               integral bounds can be oofuns as well:
+
+f5 = integrator(f1+2*f2+3*f3, (a, 10.4, f2+5*sin(f1)))
 print f5(point1), f5(point2) # Expected output: 404683.969794 107576.397664
-print f6(point1), f6(point2) # Expected output: 30394.156119 9498.22201774
+
+from numpy import inf
+f6 = integrator(1/(1+a**2+b**2+sum(c**2)), (a, 10+cos(f2+2*f3), inf))
+print f6(point1), f6(point2) # Expected output: 0.0847234400308 0.0905041349188
+
+f7 = integrator(f1+2*f2+3*sqrt(abs(f3)), (a, cos(f2+2*f3), f2+5*sin(f1)) )
 print f7(point1), f7(point2) # Expected output: 9336.70442146 5259.53130904
 
-#r_d = f4.D(point) # scipy_quad derivatives are not implemented yet
+
