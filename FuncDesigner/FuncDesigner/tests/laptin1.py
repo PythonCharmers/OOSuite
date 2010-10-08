b1374726ee2c99a323225ccdf8f1cda319a4a03a
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-10-08 06:33:56 +0000

    add another one FD test (ralg on a prob with restricted domain)
    git-svn-id: svn://openopt.org/PythonPackages@754 1214a21a-e15c-11dd-b329-07501fc18aa6

---
+++ FuncDesigner/FuncDesigner/tests/laptin1.py
@@ -0,0 +1,23 @@
+from FuncDesigner import *
+from openopt import NLP
+from numpy import arange, ones
+
+n = 50
+kappa = 1.1
+Tol = 1e+16
+
+c = arange(1,n+1)/10.0
+
+x = oovar('x')
+startPoint = {x:ones(n)}
+
+xs = sum(x)
+
+cons = (x  <= kappa/n * xs)(tol=-1e-6)
+
+f = sum(c*x) + 1e-300*sum(sqrt(-x + kappa/n * xs + Tol)) 
+
+p = NLP(f, startPoint, constraints=cons)
+
+r = p.minimize('ralg', iprint = 1000, ftol=1e-10, xtol = 1e-7, maxIter = 1e5, maxFunEvals=1e7)
+print('objective func evaluations: %d' % r.evals['f'])
