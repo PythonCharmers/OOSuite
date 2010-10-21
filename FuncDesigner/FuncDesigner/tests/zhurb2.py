9df26f38d30974a7a0e8e7af713ec9b20d5758b6
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-10-21 10:00:48 +0000

    add 2 FD NSP tests
    git-svn-id: svn://openopt.org/PythonPackages@765 1214a21a-e15c-11dd-b329-07501fc18aa6

---
+++ FuncDesigner/FuncDesigner/tests/zhurb2.py
@@ -0,0 +1,29 @@
+from numpy import arange
+from numpy.linalg import norm
+from openopt import NSP, oosolver
+from FuncDesigner import *
+
+N = 300
+x = oovar('x')
+startPoint = {x: 1 + 1.0 / arange(1, N)}
+S = 1e4 ** (1.0/arange(1, N))
+
+#f = abs(x[0]) + S * abs(x[1]) + S**2 * abs(x[2])
+f = sum(abs(x)*S)
+
+
+solvers = [oosolver('ralg')]
+solvers = [oosolver('gsubg', dual=True, zhurb = 50)]
+#solvers = [oosolver('gsubg', zhurb = 20, dual=False)]
+#solvers = ['ipopt']
+#solvers = ['slmvm2']
+#solvers = ['mma']
+for solver in solvers:
+    p = NSP(f, startPoint, maxIter = 10000, maxTime = 15000, maxFunEvals=1e7)
+    p.fEnough = 1.5e1
+    p.Ftol = 1.0e1
+    #p.constraints = (y > 5)(tol=1e-4) #x>1e-1 #[2*y<sin(arange(N))]
+    #r = p.solve(solver, iprint=10, xtol = 1e-36, ftol = 1e-16, show = solver == solvers[-1])
+    r = p.manage(solver, iprint=10, xtol = 1e-36, ftol = 1e-16, show = solver == solvers[-1])
+    
+
---
+++ FuncDesigner/FuncDesigner/tests/zhurb3.py
@@ -0,0 +1,29 @@
+from numpy import arange
+from numpy.linalg import norm
+from openopt import NSP, oosolver
+from FuncDesigner import *
+
+N = 100000
+x = oovar('x')
+startPoint = {x: 1 + 1.0 / arange(1, N+1)}
+S = 1e4 ** (1.0/arange(1, N+1))
+
+arr = sin(arange(N))
+f = sum((x-arr)**2 * S) / 1e4
+
+
+solvers = [oosolver('ralg')]
+solvers = [oosolver('gsubg', dual=True, zhurb = 50)]
+#solvers = [oosolver('gsubg', zhurb = 20, dual=False)]
+#solvers = ['ipopt']
+#solvers = ['slmvm2']
+#solvers = ['mma']
+for solver in solvers:
+    p = NSP(f, startPoint, maxIter = 10000, maxTime = 15000, maxFunEvals=1e7)
+    p.fEnough = 1.5e-1
+    p.Ftol = 1.0e-1
+    #p.constraints = (y > 5)(tol=1e-4) #x>1e-1 #[2*y<sin(arange(N))]
+    r = p.solve(solver, iprint=10, xtol = 1e-36, ftol = 1e-16, show = solver == solvers[-1])
+    #r = p.manage(solver, iprint=10, xtol = 1e-16, ftol = 1e-16, show = solver == solvers[-1])
+    
+
