89adf7019461812d8e3dd3cc1e03ae466da9d45d
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-11-27 12:33:23 +0000

    minor fix for install scripts
    git-svn-id: svn://openopt.org/PythonPackages@804 1214a21a-e15c-11dd-b329-07501fc18aa6

--- develop_all.py
+++ develop_all.py
@@ -2,5 +2,6 @@ import os, sys
 for moduleName in ['DerApproximator', 'FuncDesigner', 'OpenOpt']:
     print(moduleName + ' in-place installation:')
     os.chdir(moduleName) 
-    os.system('%s setup.py develop' % sys.executable)
+    os.system('\"%s\" setup.py develop' % sys.executable)
+    #os.system('%s setup.py develop' % sys.executable)
     os.chdir('..')
--- install_all.py
+++ install_all.py
@@ -2,5 +2,7 @@ import os, sys
 for moduleName in ['DerApproximator', 'FuncDesigner', 'OpenOpt']:
     print(moduleName + ' installation:')
     os.chdir(moduleName) 
-    os.system('%s setup.py install' % sys.executable)
+    os.system('\"%s\" setup.py install' % sys.executable)
+    #os.system('%s setup.py install' % sys.executable)
     os.chdir('..')
+
