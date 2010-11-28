b0a72a563d93946a52c9f7f9b4ad794c23027eab
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-11-28 11:31:01 +0000

    minor changes for sqlcp
    git-svn-id: svn://openopt.org/PythonPackages@811 1214a21a-e15c-11dd-b329-07501fc18aa6

--- OpenOpt/openopt/solvers/HongKongOpt/sqlcp_oo.py
+++ OpenOpt/openopt/solvers/HongKongOpt/sqlcp_oo.py
@@ -11,13 +11,16 @@ class sqlcp(baseSolver):
     __optionalDataThatCanBeHandled__ = ['A', 'Aeq', 'b', 'beq', 'lb', 'ub']
     iterfcnConnected = True
     #_canHandleScipySparse = True
-    qpsolver=None
+    QPsolver=None
     __info__ = '''SQP solver. Approximates f in x0 with paraboloid with same gradient and hessian,
     then finds its minimum with a quadratic solver (qlcp by default) and uses it as new point, 
     iterating till changes in x and/or f drop below given limits. 
     Requires the Hessian to be definite positive.
     The Hessian is initially approximated by its principal diagonal, and then
     updated at every step with the BFGS method.
+    
+    By default it uses QP solver qlcp (license: MIT), however, latter uses LCP solver LCPSolve, that is "free for education" for now. 
+    You can use other QP solver via "oosolver('sqlcp', QPsolver='cvxopt_qp')" or any other, including converters.
     
     Copyright (c) 2010 Enzo Michelangeli and IT Vision Ltd
     '''
@@ -32,7 +35,7 @@ class sqlcp(baseSolver):
             p.iterfcn(x)
             return True if p.istop else False
         try:
-            SQLCP(p.f, p.x0, df=p.df, A=A , b=b, Aeq=Aeq, beq=beq, lb=p.lb, ub=p.ub, minstep=p.xtol, minfchg=1e-15, qpsolver=self.qpsolver, callback = callback)
+            SQLCP(p.f, p.x0, df=p.df, A=A , b=b, Aeq=Aeq, beq=beq, lb=p.lb, ub=p.ub, minstep=p.xtol, minfchg=1e-15, qpsolver=self.QPsolver, callback = callback)
         except LinAlgError:
             p.msg = 'linalg error, probably failed to invert Hesse matrix'
             p.istop = -100
