bd944ace62a5f6c64b91f2aa3a1cfd718b870af2
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-12-06 14:04:01 +0000

    add a couple of tests
    git-svn-id: svn://openopt.org/PythonPackages@815 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/tests/laptin1.py
+++ FuncDesigner/FuncDesigner/tests/laptin1.py
@@ -3,7 +3,7 @@ from openopt import NLP
 from numpy import arange, ones
 
 n = 50
-kappa = 1.1
+kappa = 1.05
 Tol = 1e+16
 
 c = arange(1,n+1)/10.0
@@ -19,5 +19,5 @@ f = sum(c*x) + 1e-300*sum(sqrt(-x + kappa/n * xs + Tol))
 
 p = NLP(f, startPoint, constraints=cons)
 
-r = p.minimize('ralg', iprint = 1000, ftol=1e-10, xtol = 1e-7, maxIter = 1e5, maxFunEvals=1e7)
-print('objective func evaluations: %d' % r.evals['f'])
+r = p.minimize('gsubg', dilation = False, iprint = 10, ftol=1e-10, Ftol = 1e-4, xtol = 1e-6, maxIter = 1e5, maxFunEvals=1e7, T='float128')
+print 'objective func evaluations: ', r.evals['f']
---
+++ FuncDesigner/FuncDesigner/tests/laptin3.py
@@ -0,0 +1,26 @@
+from FuncDesigner import *
+from numpy import ones, arange
+nVar = 50
+X = oovar()
+nConstr = 2
+
+Delta = 0.00000001
+Lambda = 0.02
+Sigma = 0.00001
+
+P = 1/sqrt(nVar) * ones(nVar)
+T = sqrt(nVar)
+PX = sum(P * X)
+Norma2 = sum((X - PX * P)**2)
+F0 = Lambda * PX - sqrt(Delta**2 * PX**2 - Norma2)
+
+F1 = Norma2 + Sigma**2 - Delta**2 * PX**2
+F2 = - PX
+F_Opt = Sigma*(Lambda/Delta - 1);
+
+S = oosystem(F0) 
+S &= (F1<0,  F2<0)
+r = S.minimize(F0, {X:cos(arange(nVar))}, iprint=10, ftol = 1e-10, Ftol = 0.001, manage=True, maxFunEvals = 1e9, 
+                                 xtol = 1e-8, T='float128', maxIter = 1e5, contol=1e-12, solver='gsubg')
+print('Obtained objfunc result: %f   Theoretical: %f' % (F0(r), F_Opt))
+
