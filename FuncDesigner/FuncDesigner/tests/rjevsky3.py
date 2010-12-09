654cd69d8b7c0dfab30876465de29b3b5fd83a73
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-12-09 10:10:22 +0000

    minor fix for FD test
    git-svn-id: svn://openopt.org/PythonPackages@824 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/tests/rjevsky3.py
+++ FuncDesigner/FuncDesigner/tests/rjevsky3.py
@@ -3,7 +3,7 @@ from numpy.linalg import norm
 from openopt import NSP, oosolver
 from FuncDesigner import *
 
-n = 5000
+n = 20
 x = oovar()
 X = 1.0 / n * ones(n)
 T = (0.01 * arange(1, 101))
@@ -12,17 +12,17 @@ for j in range(100):
     tmp.append(abs((x-X) * T[j]**arange(n)))
 obj = sum(tmp)
 
-#startPoint = {x: zeros(n)}
-startPoint = {x: cos(arange(n))/n}
+startPoint = {x: zeros(n)}
+#startPoint = {x: cos(arange(n))/n}
 
 #solvers = [gsubg, 'ralg', 'ipopt']
-solvers = [gsubg]
+solvers = ['gsubg']
 
 Colors = ['r', 'k','b']
 
 lines = []
 for i, solver in enumerate(solvers):
     p = NSP(obj, startPoint, maxIter = 1700, name = 'rjevsky3 (nVars: ' + str(n)+')', maxTime = 300, maxFunEvals=1e7, color = Colors[i])
-    p.fEnough = 1.0e-2
-    p.Ftol = 0.5e-2
-    r = p.manage(solver, iprint=1, xtol = 1e-6, ftol = 1e-6, debug=0, show = solver == solvers[-1], plot = 0)
+    p.fEnough = 1.0e-5
+    p.Ftol = 0.5e-5
+    r = p.manage(solver, iprint=1, xtol = 1e-10, ftol = 1e-10, debug=0, show = solver == solvers[-1], plot = 0)
