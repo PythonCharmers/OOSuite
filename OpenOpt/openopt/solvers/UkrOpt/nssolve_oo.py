905ac26d085b6fc88cffc19bdd0c7292e099bb06
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2010-12-29 15:12:52 +0000

    some nssolve changes
    git-svn-id: svn://openopt.org/PythonPackages@849 1214a21a-e15c-11dd-b329-07501fc18aa6

--- OpenOpt/openopt/solvers/UkrOpt/nssolve_oo.py
+++ OpenOpt/openopt/solvers/UkrOpt/nssolve_oo.py
@@ -36,19 +36,36 @@ class nssolve(baseSolver):
         if SMALL_DELTA_X in p.kernelIterFuncs.keys(): p.kernelIterFuncs.pop(SMALL_DELTA_X)
         if SMALL_DELTA_F in p.kernelIterFuncs.keys(): p.kernelIterFuncs.pop(SMALL_DELTA_F)
 
-        f = lambda x: max(abs(p.f(x)))
-        def df(x):
-            F = p.f(x)
-            ind = argmax(abs(F))
-            return p.df(x, ind) * sign(F[ind])
-            
-#        f = lambda x: sum(abs(p.f(x)))
-#        def df(x):
-#            return dot(p.df(x),  sign(p.f(x)))
-            
+        if self.nspSolver == 'autoselect':
+            nspSolver = 'amsg2p' if p.isUC else 'ralg'
+        else:
+            nspSolver = self.nspSolver
+        
+#        nspSolver = 'ralg'
+
+        way = 3 if nspSolver == 'ralg' else 2
+       
+        if way == 1:
+            use2 = False
+            f = lambda x: sum(abs(p.f(x)))
+            def df(x):
+                return dot(p.df(x),  sign(p.f(x)))
+        elif way == 2:
+            use2 = True
+            f = lambda x: sum(p.f(x)**2)
+            def df(x):
+                return 2.0*dot(p.f(x),  p.df(x))
+        elif way == 3:
+            use2 = False
+            f = lambda x: max(abs(p.f(x)))
+            def df(x):
+                F = p.f(x)
+                ind = argmax(abs(F))
+                return p.df(x, ind) * sign(F[ind])
+
+        FTOL = p.ftol**2 if use2 else p.ftol
         def iterfcn(*args,  **kwargs):
             p2.primalIterFcn(*args,  **kwargs)
-            #p2.solver.__decodeIterFcnArgs__(p2,  *args,  **kwargs)
             p.xk = p2.xk.copy()
             Fk = norm(p.f(p.xk), inf)
             p.rk = p.getMaxResidual(p.xk)
@@ -58,20 +75,20 @@ class nssolve(baseSolver):
             if p.nEvals['f'] > p.maxFunEvals:
                 p.istop = p2.istop = IS_MAX_FUN_EVALS_REACHED
             elif p2.istop!=0:
-                if Fk < p.ftol and p.rk < p.contol:
+                if Fk < FTOL and p.rk < p.contol:
                     p.istop = 15
-                    if p.isUC: msg_contol = ''
-                    else: msg_contol = 'and contol '
+                    msg_contol = '' if p.isUC else 'and contol '
                     p.msg = 'solution with required ftol ' + msg_contol+ 'has been reached'
                 else: p.istop = p2.istop
 
             p.iterfcn()
+            
             return p.istop
 
 
         p2 = NSP(f, p.x0, df=df, xtol = p.xtol/1e16, gtol = p.gtol/1e16,\
         A=p.A,  b=p.b,  Aeq=p.Aeq,  beq=p.beq,  lb=p.lb,  ub=p.ub, \
-        maxFunEvals = p.maxFunEvals, fEnough = p.ftol, maxIter=p.maxIter, iprint = -1, \
+        maxFunEvals = p.maxFunEvals, fEnough = FTOL, maxIter=p.maxIter, iprint = -1, \
         maxtime = p.maxTime, maxCPUTime = p.maxCPUTime,  noise = p.noise, fOpt = 0.0)
 
         if p.userProvided.c:
@@ -83,12 +100,6 @@ class nssolve(baseSolver):
 
         if p.debug: p2.iprint = 1
         
-        if self.nspSolver == 'autoselect':
-            nspSolver = 'amsg2p' if p.isUC else 'ralg'
-        else:
-            nspSolver = self.nspSolver
-        
-        #nspSolver = 'ralg'
         if nspSolver == 'ralg':
             if p.isUC: p2.ftol = p.ftol / 1e16
             else: p2.ftol = 0.0
@@ -96,7 +107,10 @@ class nssolve(baseSolver):
             p2.ftol = 0.0
             p2.xtol = 0.0
             p2.gtol = 0.0
-        p2.Ftol = p.ftol
+        if use2:
+            p2.Ftol = p.ftol ** 2
+        else:
+            p2.Ftol = p.ftol
         r2 = p2.solve(nspSolver)
         #xf = fsolve(p.f, p.x0, fprime=p.df, xtol = p.xtol, maxfev = p.maxFunEvals)
         xf = r2.xf
