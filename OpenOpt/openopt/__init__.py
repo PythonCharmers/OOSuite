577e285611c1f158db007a988a5815d396bfef45
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2011-02-11 20:45:35 +0000

    minor change
    git-svn-id: svn://openopt.org/PythonPackages@933 1214a21a-e15c-11dd-b329-07501fc18aa6

--- OpenOpt/openopt/__init__.py
+++ OpenOpt/openopt/__init__.py
@@ -7,16 +7,18 @@ from kernel.GUI import manage
 from kernel.oologfcn import OpenOptException
 from kernel.nonOptMisc import oosolver
 
-import os
-if 'EPD' in os.path.abspath( __file__ ):
+try:
+    import enthought
     s = """
-    You are using Enthought Python Distribution; 
+    Seems like you are using Enthought Python Distribution; 
     consider using free GPL-licensed alternatives 
     PythonXY (http://www.pythonxy.com) or
     Sage (http://sagemath.org) instead.
     """
     print(s)
-
+except ImportError:
+    pass
+    
 #__all__ = filter(lambda s:not s.startswith('_'),dir())
 
 #from numpy.testing import NumpyTest
