6d53c19f0796a2d160fa37a27d8510b1296cb598
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2011-02-15 18:28:35 +0000

    add SF polygon example
    git-svn-id: svn://openopt.org/PythonPackages@947 1214a21a-e15c-11dd-b329-07501fc18aa6

---
+++ SpaceFuncs/examples/polygon.py
@@ -0,0 +1,19 @@
+from FuncDesigner import *
+from SpaceFuncs import *
+spaceDimension = 2
+nVertices = 15
+parametrizedVerticesCoords = oovars(nVertices, size=spaceDimension)# Python list of 15 oovars, each one of size spaceDimension
+P = Polygon(parametrizedVerticesCoords)
+
+parameterValues = dict([(parametrizedVerticesCoords[i], (sin(2 * pi * i / nVertices), cos(2 * pi  * i  / nVertices))) for i in range(nVertices)])
+print(P.sides(parameterValues))
+print(P.P(parameterValues))# perimeter, 6.23735072453
+print(P.S(parameterValues))# 3.05052482307
+areaPlusPerimeter = P.S + P.P
+
+print('for these params area + perimeter equals to ' + str(areaPlusPerimeter(parameterValues)))# 9.2878755476 for nVertices = 15
+
+Plot(P(parameterValues))
+
+
+
