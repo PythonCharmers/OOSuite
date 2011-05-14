be9ef2f9db4c270da302b815ce2d00e95f89a8ab
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2011-05-14 09:44:19 +0000

    oo fix
    git-svn-id: svn://openopt.org/PythonPackages@1066 1214a21a-e15c-11dd-b329-07501fc18aa6

--- OpenOpt/openopt/kernel/nonOptMisc.py
+++ OpenOpt/openopt/kernel/nonOptMisc.py
@@ -22,7 +22,7 @@ try:
         s = set([(0 if isscalar(elem) else elem.ndim) for elem in elems])
         ndim = max(s)
         if ndim <= 1:  return hstack(elems)
-        assert ndim <= 2 and 1 not in s, 'bug in OpenOpt kernel, inform developers'
+        #assert ndim <= 2 and 1 not in s, 'bug in OpenOpt kernel, inform developers'
         return hstack(elems) if 0 not in s else hstack([atleast_2d(elem) for elem in elems])
         
     def Vstack(Tuple):
@@ -34,7 +34,7 @@ try:
         s = set([(0 if isscalar(elem) else elem.ndim) for elem in elems])
         ndim = max(s)
         if ndim <= 1:  return vstack(elems)
-        assert ndim <= 2 and 1 not in s, 'bug in OpenOpt kernel, inform developers'
+        #assert ndim <= 2 and 1 not in s, 'bug in OpenOpt kernel, inform developers'
         return vstack(elems) if 0 not in s else vstack([atleast_2d(elem) for elem in elems])
         
     #Hstack = lambda Tuple: HstackSP(Tuple) if any([isspmatrix(elem) for elem in Tuple]) else hstack(Tuple)
