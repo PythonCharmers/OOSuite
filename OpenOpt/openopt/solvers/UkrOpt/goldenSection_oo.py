f85cf6181323b0d6466f634f98e56356e50288c4
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2011-12-15 10:16:30 +0000

    minor change
    git-svn-id: svn://openopt.org/PythonPackages@1296 1214a21a-e15c-11dd-b329-07501fc18aa6

--- OpenOpt/openopt/solvers/UkrOpt/goldenSection_oo.py
+++ OpenOpt/openopt/solvers/UkrOpt/goldenSection_oo.py
@@ -1,6 +1,6 @@
 from openopt.kernel.baseSolver import baseSolver
 from openopt.kernel.setDefaultIterFuncs import SMALL_DELTA_X, IS_MAX_ITER_REACHED, SMALL_DELTA_F
-from numpy import nan, diff
+from numpy import nan, diff, copy
 
 class goldenSection(baseSolver):
     __name__ = 'goldenSection'
@@ -22,7 +22,7 @@ class goldenSection(baseSolver):
         if p.n != 1: p.err('the solver ' + self.__name__ +' can handle singe-variable problems only')
         if not p.__isFiniteBoxBounded__(): p.err('the solver ' + self.__name__ +' requires finite lower and upper bounds')
         if SMALL_DELTA_X in p.kernelIterFuncs.keys(): p.kernelIterFuncs.pop(SMALL_DELTA_X)
-        a,  b,  f = p.lb,  p.ub,  p.f
+        a,  b,  f = copy(p.lb),  copy(p.ub),  p.f
         if self.rightBorderForLocalization is None: self.rightBorderForLocalization = p.ub
         if self.leftBorderForLocalization is None: self.leftBorderForLocalization = p.lb
 
