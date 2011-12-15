d8658036ab71af76fa75832830916f37d1c570ae
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2011-12-15 09:57:14 +0000

    minor install routines changes
    git-svn-id: svn://openopt.org/PythonPackages@1295 1214a21a-e15c-11dd-b329-07501fc18aa6

--- develop_all.py
+++ develop_all.py
@@ -1,3 +1,11 @@
+try:
+    import setuptools
+except:
+    print('you should have setuptools installed (http://pypi.python.org/pypi/setuptools), for some Linux distribs you can get it via [sudo] apt-get install python-setuptools')
+    print('press Enter for exit...')
+    raw_input()
+    exit()
+
 import os, sys
 for moduleName in ['DerApproximator', 'FuncDesigner', 'OpenOpt', 'SpaceFuncs']:
     print(moduleName + ' in-place installation:')
