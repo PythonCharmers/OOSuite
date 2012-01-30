5676e66154ebeaad00770f4d4e0eab459c027627
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2012-01-30 17:40:08 +0000

    minor change for interalg example
    git-svn-id: svn://openopt.org/PythonPackages@1378 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/exactGlobalMINLP.py
+++ FuncDesigner/FuncDesigner/examples/exactGlobalMINLP.py
@@ -6,6 +6,8 @@ from openopt import *
  
 n = 7
 x = oovars(n)
+for i in range(n):
+    x[i](str(i))
 x[4].domain = [0.9, -0.7, 0.4] # let's set one of x coords as  discrete variable
 y = oovar(domain = bool) # the same to domain = [0,1]
 z = oovar(domain = {1, 1.5, 2, 4, 6, -4, 5}) # for obsolete Python versions use [] or () instead of {}
@@ -16,10 +18,10 @@ constraints = [
                x>-1, x<1, 
                (x[0]**2 + x[1]**2 == 0.5)(tol=1.0e-7), 
                (x[1]**2 + 0.71*x[1]**2 + (x[2]-0.02)**2 <= 0.17)(tol=1e-4), 
-               (x[2]-0.1)**2 + (x[3]-0.03)**2 <= 0.01,  # default constraint tol is 10^-6
+               (x[2]-0.1)**2 + (x[3]-0.03)**2 <= 0.0009,  # default constraint tol is 10^-6
                cos(y) + x[5] <= -0.1, 
                z**2 +arctan(x[1]) < 16, 
-               interpolator([1, 2, 3, 4], [1.001, 4, 9, 16.01])(x[4]+2*x[5]) < 6
+               interpolator([1, 2, 3, 4], [1.001, 4, 9, 16.01])(3+x[4]+x[5]) < 20
                ]
 
 startPoint = {x:[0]*n, y:0, z:1.5} # [0]*n means Python list [0,0,...,0] with n zeros
@@ -42,16 +44,13 @@ print(r(x, y, z))
 solver: interalg   problem: unnamed    type: MINLP   goal: minimum
  iter   objFunVal   log10(MaxResidual/ConTol)   
     0  9.890e-01                      6.70 
-  100  -2.571e-01                     -1.20 
-  200  -2.910e-01                     -0.47 
-  300  -2.910e-01                     -0.47 
-  400  -2.926e-01                     -0.22 
+  100  -2.616e-01                     -0.37 
 OpenOpt info: Solution with required tolerance 5.0e-04 
  is guarantied (obtained precision: 5.0e-04)
-  409  -2.926e-01                     -0.22 
+  190  -2.669e-01                     -0.15 
 istop: 1000 (solution has been obtained)
-Solver:   Time Elapsed = 37.75 	CPU Time Elapsed = 37.01
-objFunValue: -0.29255101 (feasible, max(residuals/requiredTolerances) = 0.59926)
-[[-0.63428688049307214, -0.31253814696507071, 0.074464532391391849, -0.066528274992712286, 
--0.69999999999999996, -0.88575384524595291, 0.89992401222574525], 1.0, 4.0]
+Solver:   Time Elapsed = 12.42 	CPU Time Elapsed = 11.86
+objFunValue: -0.26687054 (feasible, max(residuals/requiredTolerances) = 0.709678)
+[[-0.63549995422337446, -0.31006431453795402, 0.0947155871999006, 0.0004902022309690152, 
+-0.69999999999999996, -0.99903866874298164, 0.89991287060890723], 1.0, 4.0]
 '''
