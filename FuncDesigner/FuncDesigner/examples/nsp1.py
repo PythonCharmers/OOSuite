15719a1ba13ec1dbdb957e2c5097e7f6f0717db4
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2012-02-24 16:36:46 +0000

    minor change for FD NSP example
    git-svn-id: svn://openopt.org/PythonPackages@1441 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/nsp1.py
+++ FuncDesigner/FuncDesigner/examples/nsp1.py
@@ -8,7 +8,7 @@ N = 75
 koeffs = arange(1, N+1) ** 1.2 # 1, 1.2, 1.44, ..., 1.2^m, ..., 1.2^N
 
 objective = sum(abs(x) * koeffs) + abs(y-15) + abs(y+15) + y**2
-constraints = [(y-1)**2<1, abs(y) < 0.5]
+constraints = [(y-1)**2<1, abs(y) < 0.5, abs(x[0]) < 1e-5, abs(x[N-1]) < 1e-5]
 constraints.append((x - 0.01*arange(N))**2 < 0.1*arange(1, N+1)) # (x_0-0)**2 < 0.1, (x_1-0.01)**2 < 0.2, (x_2-0.02)**2 < 0.2,...
 startPoint = {x: cos(1+arange(N)), y:80}
 
@@ -21,9 +21,9 @@ print(max(abs(x_opt)), y_opt)
 '''
 expected output:
 [...]
-  787  3.016e+01            -100.00 
+  876  3.004e+01            -100.00 
 istop: 4 (|| F[k] - F[k-1] || < ftol)
-Solver:   Time Elapsed = 6.99 	CPU Time Elapsed = 6.7
-objFunValue: 30.16499 (feasible, MaxResidual = 0)
-(1.0427339807505488e-05, 0.40586000195265448)
+Solver:   Time Elapsed = 7.98 	CPU Time Elapsed = 7.97
+objFunValue: 30.042539 (feasible, MaxResidual = 0)
+(6.6277698279489041e-06, 0.20306221768582972)
 '''
