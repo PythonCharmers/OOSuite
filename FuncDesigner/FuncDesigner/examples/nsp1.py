a77a5b413ff306a06a5485f4ff677cd19cd8fe37
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2012-02-24 16:20:36 +0000

    change for FD nsp example
    git-svn-id: svn://openopt.org/PythonPackages@1440 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/nsp1.py
+++ FuncDesigner/FuncDesigner/examples/nsp1.py
@@ -1,32 +1,29 @@
-"""
-Example for NSP 
-|x[0]| + 1.2*|x[1]| + 1.44*|x[2]| + ... + 1.2^74*|x[74]| + abs(y-15) +abs(y+15) + y^2 +  -> min
-"""
+from numpy import cos, arange
 from FuncDesigner import *
 from openopt import NSP
-from numpy import cos, arange
 
-x, y = oovars('x', 'y')
+x, y = oovars('x y')
 
 N = 75
-koeffs = arange(1, N+1) ** 1.2
+koeffs = arange(1, N+1) ** 1.2 # 1, 1.2, 1.44, ..., 1.2^m, ..., 1.2^N
 
-f = (abs(x) * koeffs).sum() + abs(y-15) + abs(y+15) + y**2
-f /= 1e2
+objective = sum(abs(x) * koeffs) + abs(y-15) + abs(y+15) + y**2
+constraints = [(y-1)**2<1, abs(y) < 0.5]
+constraints.append((x - 0.01*arange(N))**2 < 0.1*arange(1, N+1)) # (x_0-0)**2 < 0.1, (x_1-0.01)**2 < 0.2, (x_2-0.02)**2 < 0.2,...
 startPoint = {x: cos(1+arange(N)), y:80}
 
-p = NSP(f, startPoint, maxIter = 1e5)
+p = NSP(objective, startPoint, maxIter = 1e5, constraints = constraints)
 
-#r = p.solve('ralg')
-p.fTol = 1.0
-r = p.manage('gsubg', zhurb = 150, xtol = 1e-17, ftol = 1e-17, maxFunEvals = 1e9)
-x_opt, y_opt = r(x, y)
+r = p.solve('ralg')
+x_opt, y_opt = x(r), y(r)
 print(max(abs(x_opt)), y_opt)
-"""
-...
-  990  3.000e+01 
-istop: 4(|| F[k] - F[k-1] || < ftol)
-Solver:   Time Elapsed = 11.3 	CPU Time Elapsed = 10.88
-objFunValue: 30.000409
-(2.3481047305075244e-06, array([  6.18404460e-08]))
-"""
+
+'''
+expected output:
+[...]
+  787  3.016e+01            -100.00 
+istop: 4 (|| F[k] - F[k-1] || < ftol)
+Solver:   Time Elapsed = 6.99 	CPU Time Elapsed = 6.7
+objFunValue: 30.16499 (feasible, MaxResidual = 0)
+(1.0427339807505488e-05, 0.40586000195265448)
+'''
