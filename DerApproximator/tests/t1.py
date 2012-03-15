0282fec61755f8f9ee302713cca56106444a0091
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2012-03-15 09:27:11 +0000

    minor change in DA example for Python3 compatibility
    git-svn-id: svn://openopt.org/PythonPackages@1500 1214a21a-e15c-11dd-b329-07501fc18aa6

--- DerApproximator/tests/t1.py
+++ DerApproximator/tests/t1.py
@@ -2,9 +2,9 @@ from DerApproximator import *
 import numpy as np
 np.seterr(invalid='ignore')
 
-print get_d1(lambda x: (x**2).sum(), [1,2,3])
-print get_d1(lambda x: x**2, [1,2,3])
-print get_d1(lambda x: ((-x)**0.5)**2, [0], stencil=1)
+print(get_d1(lambda x: (x**2).sum(), [1,2,3]))
+print(get_d1(lambda x: x**2, [1,2,3]))
+print(get_d1(lambda x: ((-x)**0.5)**2, [0], stencil=1))
 '''
 [ 1.99999993  3.99999998  6.0000002 ]
 [[ 1.99999998  0.          0.        ]
