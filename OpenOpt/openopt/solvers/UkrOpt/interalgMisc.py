ca0c3d508eaadba0c83a82e311e986914d10ab1d
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2012-05-04 13:28:15 +0000

    minor changes for interalg
    git-svn-id: svn://openopt.org/PythonPackages@1562 1214a21a-e15c-11dd-b329-07501fc18aa6

--- OpenOpt/openopt/solvers/UkrOpt/interalgMisc.py
+++ OpenOpt/openopt/solvers/UkrOpt/interalgMisc.py
@@ -95,15 +95,26 @@ def r14(p, nlhc, residual, definiteRange, y, e, vv, asdf1, C, r40, itn, g, nNode
             tnlh_curr = tnlh_fixed
         else:
             if fo != fo_prev:
-                fos = asarray([node.fo for node in an])
-                mino = asarray([node.key for node in an])
-                ind = where(fos > fo + 0.01* fTol)[0]
-                if any(ind):# elseware bug with shapes of zero-sized arrays
-                    o_tmp, a_tmp = asarray([an[i].o for i in ind]), asarray([an[i].a for i in ind])
+                if fo < fo_prev - fTol:
+                    update_nlh = True
+                    TF = tnlh_fixed
+                    o_tmp, a_tmp = array([node.o for node in an]), array([node.a for node in an])
+                    nodesToUpdate = an
+                else:
+                    fos = asarray([node.fo for node in an])
+                    ind = where(fos > fo + 0.01* fTol)[0]
+                    update_nlh = True if ind.size != 0 else False
+                    TF = tnlh_fixed[ind]
+                    o_tmp, a_tmp = array([an[i].o for i in ind]), array([an[i].a for i in ind])
+                    nodesToUpdate = an[ind]
+                
+                if update_nlh:
+                    
                     a_tmp[a_tmp>fo] = fo                
-                    tnlh_all_new = tnlh_fixed[ind] - log2(a_tmp - o_tmp)
+                    tnlh_all_new = TF - log2(a_tmp - o_tmp)
+                    
                     tnlh_curr_best = nanmin(tnlh_all_new, 1)
-                    for j, node in enumerate(an[ind]): 
+                    for j, node in enumerate(nodesToUpdate): 
                         node.fo = fo
                         node.tnlh_curr = tnlh_all_new[j]
                         node.tnlh_curr_best = tnlh_curr_best[j]
