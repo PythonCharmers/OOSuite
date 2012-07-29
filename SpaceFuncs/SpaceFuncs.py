80b432b6170a68ba6c60be6c42c410c4fde65996
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2012-07-29 18:41:06 +0000

    some sf changes for better Python3 compatibility
    git-svn-id: svn://openopt.org/PythonPackages@1712 1214a21a-e15c-11dd-b329-07501fc18aa6

--- SpaceFuncs/SpaceFuncs.py
+++ SpaceFuncs/SpaceFuncs.py
@@ -3,9 +3,9 @@ import os,sys
 Sep = os.sep
 sfPath = ''.join(elem+Sep for elem in __file__.split(Sep)[:-1])
 sys.path.append(sfPath + 'kernel')
-from kernel.baseObjects import Point, Line, LineSegment, Plane,  Circle, Disk, skewLinesNearestPoints, Sphere
+from baseObjects import Point, Line, LineSegment, Plane,  Circle, Disk, skewLinesNearestPoints, Sphere, Ball
 for name in ['Triangle', 'Tetrahedron', 'Polygon', 'Polytope']:
-    exec('from kernel.%s import %s' % (name, name))
+    exec('from %s import %s' % (name, name))
 #from kernel.Triangle import Triangle
 from kernel.Plot import Plot
 from numpy import pi
