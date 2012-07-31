f566d4117aad64b595f38e998f2262926d6a2e19
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2012-07-31 14:55:27 +0000

    some FD fixes
    git-svn-id: svn://openopt.org/PythonPackages@1718 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/ooPoint.py
+++ FuncDesigner/FuncDesigner/ooPoint.py
@@ -8,7 +8,7 @@
 
 from FDmisc import FuncDesignerException
 from baseClasses import Stochastic
-from numpy import asanyarray, ndarray, isscalar, array
+from numpy import asanyarray, ndarray, isscalar, array, atleast_2d, sum as np_sum
 try:
     from scipy.sparse import isspmatrix
 except ImportError:
@@ -34,24 +34,33 @@ class multiarray(ndarray):
     __pow__ = lambda self, other: multiarray_op(self, other, operator.pow)
     __rpow__ = lambda self, other: multiarray_op(other, self, operator.rpow)
     toarray = lambda self: self.view(ndarray)
+    #sum = lambda self: atleast_2d(self).sum(axis = 1)
+    def sum(self, *args, **kw):
+        if any([v is not None for v in args]): # somehow triggered from pswarm
+            raise FuncDesignerException('arguments for FD multiarray sum are not implemened yet')
+        if any([v is not None for v in kw.values()]):
+            raise FuncDesignerException('keyword arguments for FD multiarray sum are not implemened yet')
+        #print('in multiarray sum')
+        return np_sum(atleast_2d(self.view(ndarray)), 0).view(multiarray)
 
 def multiarray_op(x, y, op):
     if isinstance(y, multiarray):
-        #or isscalar(y) or isscalar(x) or 
-        r = op(x.view(ndarray), y.view(ndarray))
-    elif isscalar(y) or (isinstance(y, ndarray) and y.size == 1):
-        r = op(x.view(ndarray), y).view(multiarray)
-    elif isscalar(x) or (isinstance(x, ndarray) and x.size == 1):
-        r = op(x, y.view(ndarray))
-    else:
-        r =  array([(elem + y) for elem in x.view(ndarray)])
+        if isinstance(x, multiarray):
+            if x.size == y.size:
+                r = op(x.view(ndarray), y.view(ndarray))
+            else:
+                assert x.ndim <= 1 or y.ndim <= 1, 'unimplemented yet'
+                assert x.ndim < 3 and y.ndim < 3, 'unimplemented yet'
+                X, Y = atleast_2d(x).view(ndarray), atleast_2d(y).view(ndarray)
+                r = op(X, Y)
+                #r = multiarray([op(x[i], y[i]) for i, X in enumerate(x)])
+        else:
+            r = op(x.reshape(-1, 1) if isinstance(x, ndarray) else x, y.view(ndarray))
+    elif isinstance(x, multiarray): # and y is not multiarray here
+        r = op(x.view(ndarray), y.reshape(-1, 1) if isinstance(y, ndarray) else y)
+    else: # neither x nor y are multiarrays
+        raise FuncDesignerException('bug in FuncDesigner kernel')
     return r.view(multiarray)
-#        try:
-#            return array([(elem + y) for elem in x.view(ndarray)]).view(multiarray)
-#        except:
-#            print x.shape
-#            print y,  type(y)
-#            raise 0
     
 
 def ooMultiPoint(*args, **kw):
