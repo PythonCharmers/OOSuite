b3d49bd8deec5c8aa2827be7aa7a2eff1f13c467
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2013-03-15 11:43:05 +0000

    minor sf cleanup +  version change
    git-svn-id: svn://openopt.org/PythonPackages@1892 1214a21a-e15c-11dd-b329-07501fc18aa6

--- SpaceFuncs/SpaceFuncs.py
+++ SpaceFuncs/SpaceFuncs.py
@@ -6,6 +6,5 @@ sys.path.append(sfPath + 'kernel')
 from baseObjects import Point, Line, LineSegment, Plane,  Circle, Disk, skewLinesNearestPoints, Sphere, Ball
 for name in ['Triangle', 'Tetrahedron', 'Polygon', 'Polytope']:
     exec('from %s import %s' % (name, name))
-#from kernel.Triangle import Triangle
 from kernel.Plot import Plot
 from numpy import pi
