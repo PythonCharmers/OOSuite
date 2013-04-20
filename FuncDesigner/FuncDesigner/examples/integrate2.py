c6147fb3e98f9369d4df786314cdf8a221083706
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2013-04-20 09:53:29 +0000

    minor fd doc change
    git-svn-id: svn://openopt.org/PythonPackages@1971 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/integrate2.py
+++ FuncDesigner/FuncDesigner/examples/integrate2.py
@@ -1,9 +1,11 @@
 from numpy import pi
 
-sigma = 1e-3
+sigma = 1e-4
 # here lambda y, x is used to keep it in accordance with scipy.integrate.dblquad API
 # for FuncDesigner models you shouldn't keep the order in mind
 ff = lambda y, x:  (exp(-(x-0.1)**2/(2*sigma)) * exp(-(y+0.2)**2/(2*sigma))) / (2*pi*sigma) 
+#ff = lambda y, x:  (exp(-(x-0.1)**2/(2*sigma)) * exp(-(y+0.2)**2/(2*sigma))) / (2*pi*sigma) 
+
 
 bounds_x = (-15, 5)
 bounds_y = (-15, 5)
@@ -12,17 +14,17 @@ from FuncDesigner import *
 from openopt import IP
 x, y = oovars('x y') 
 f = ff(y, x) 
-# or 
+# or 00
 #f = (exp(-(x-0.1)**2/(2*sigma)) * exp(-(y+0.2)**2/(2*sigma))) / (2*pi*sigma) 
 
 domain = {x: bounds_x, y: bounds_y}
 p = IP(f, domain, ftol = 0.05)
-r = p.solve('interalg', maxIter = 15000, maxNodes = 500000, maxActiveNodes = 150, iprint = 100)
+r = p.solve('interalg', maxIter = 50, maxNodes = 500000, maxActiveNodes = 150, iprint = 100)
 print('interalg result: %f' % r.ff)
 '''
 OpenOpt Suite 0.45+ on Intal Atom 1.7 GHz:
-Solver:   Time Elapsed = 10.36 	CPU Time Elapsed = 10.29
-objFunValue: 1.0001709 (feasible, MaxResidual = 0.0469034)
+Solver:   Time Elapsed = 1.34 	CPU Time Elapsed = 1.34
+objFunValue: 1.001662 (feasible, MaxResidual = 0.0369961)
 (usually solution, obtained by interalg, has real residual 10-100 times less 
 than required tolerance, because interalg works with "most worst case" that extremely rarely occurs. 
 Unfortunately, real obtained residual cannot be revealed).
