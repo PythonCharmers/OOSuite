6ffca21d394a57276c652c4ca8fb7c60549fe429
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2013-06-25 16:20:58 +0000

    minor fd example change
    git-svn-id: svn://openopt.org/PythonPackages@2100 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/examples/minlp1.py
+++ FuncDesigner/FuncDesigner/examples/minlp1.py
@@ -3,16 +3,15 @@ from openopt import MINLP
 
 a, b, c = oovars('a', 'b', 'c')
 d = oovar('d', domain = [1, 2, 3, 3.5, -0.5,  -4]) # domain should be Python list/set/tuple of allowed values
-startPoint = {a:[100, 12], b:2, c:40, d:0} # however, you'd better use numpy arrays instead of Python lists
-
-f = sum(a*[1, 2])**2 + b**2 + c**2 + d**2
+e = oovar('e', domain = [1, 2, 3, 3.5, -0.5,  -4])
+a=oovars(2)('a')
+startPoint = {a:[100, 12], b:2, c:40, d:1, e:2} # however, you'd better use numpy arrays instead of Python lists
+f = sum(a*[1, 2])**2 + b**2 + c**2 + d**2+e**2
 constraints = [(2*c+a-10)**2 < 1.5 + 0.1*b, (a-10)**2<1.5, a[0]>8.9, (a+b > [ 7.97999836, 7.8552538 ])('sum_a_b', tol=1.00000e-12), \
 a < 9, b < -1.02, c > 1.01, ((b + c * log10(a).sum() - 1) ** 2==0)(tol=1e-6), b+d**2 < 100]
 
 p = MINLP(f, startPoint, constraints = constraints)
-r = p.minimize('branb', nlpSolver='ralg', plot=0, discrtol = 1e-6, xtol=1e-7)
-#r = p.solve('ralg') # for NLPs old-style (openopt 0.25 and below) p.solve() is same to p.minimize()
-#r = p.maximize('ralg')
+r = p.minimize('interalg', iprint=10, fTol = 1e-8, implicitBounds=1000)
 print(r.xf)
 a_opt,  b_opt, c_opt, d_opt = r(a, b, c, d)
 # or any of the following: 
@@ -24,6 +23,8 @@ a_opt,  b_opt, c_opt, d_opt = r(a, b, c, d)
 """
 Expected output:
 ...
-objFunValue: 718.00734 (feasible, max(residuals/requiredTolerances) = 0.135819)
-{a: array([ 8.99999879,  8.87525369]), b: array([-1.01999986]), c: array([ 1.06177278]), d: array([-0.5])}
+OpenOpt info: Solution with required tolerance 1.0e-08 is guarantied
+Solver:   Time Elapsed = 4.72 	CPU Time Elapsed = 4.48
+objFunValue: 718.2574 (feasible, max(residuals/requiredTolerances) = 0.00144128)
+{b: -1.0200000004647196, c: 1.0618036990619659, d: -0.5, e: -0.5, a_0: 8.9999983590234365, a_1: 8.8752537991469964}
 """
