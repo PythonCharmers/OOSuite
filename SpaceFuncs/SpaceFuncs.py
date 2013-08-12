558bd2682c80e689c5463c8d2da39e22c89e7848
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2013-08-12 07:57:06 +0000

    minor sf fix
    git-svn-id: svn://openopt.org/PythonPackages@2214 1214a21a-e15c-11dd-b329-07501fc18aa6

--- SpaceFuncs/SpaceFuncs.py
+++ SpaceFuncs/SpaceFuncs.py
@@ -1,10 +1,6 @@
 # created by Dmitrey
-import os,sys
-Sep = os.sep
-sfPath = ''.join(elem+Sep for elem in __file__.split(Sep)[:-1])
-sys.path.append(sfPath + 'kernel')
-from baseObjects import Point, Line, LineSegment, Plane,  Circle, Disk, skewLinesNearestPoints, Sphere, Ball
-for name in ['Triangle', 'Tetrahedron', 'Polygon', 'Polytope']:
-    exec('from %s import %s' % (name, name))
-from kernel.Plot import Plot
+
+from kernel.baseObjects import Point, Line, LineSegment, Plane,  Circle, Disk, skewLinesNearestPoints, Sphere, Ball
+for name in ['Triangle', 'Tetrahedron', 'Polygon', 'Polytope', 'Plot']:
+    exec('from kernel.%s import %s' % (name, name))
 from numpy import pi
