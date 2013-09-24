04d22d414b89f4d4607a80a4de26a8c077ca77be
dmitrey <dmitrey@1214a21a-e15c-11dd-b329-07501fc18aa6>
2013-09-24 16:36:13 +0000

    some fd changes
    git-svn-id: svn://openopt.org/PythonPackages@2336 1214a21a-e15c-11dd-b329-07501fc18aa6

--- FuncDesigner/FuncDesigner/ooFun.py
+++ FuncDesigner/FuncDesigner/ooFun.py
@@ -4,8 +4,8 @@ PythonAny = any
 PythonMax = max
 from numpy import inf, asfarray, all, any, atleast_2d, zeros, dot, asarray, atleast_1d, \
 ones, ndarray, where, array, nan, vstack, eye, array_equal, isscalar, log, hstack, sum as npSum, prod, nonzero,\
-asscalar, zeros_like, ones_like, logical_and, logical_or, \
-tile, float64, searchsorted, int8, int16, int32, int64, string_, asanyarray, bool_
+asscalar, zeros_like, ones_like, logical_and, \
+tile, searchsorted, int8, int16, int32, int64, string_, asanyarray
 
 import operator, numpy as np
 
@@ -17,24 +17,21 @@ except ImportError:
 
 from expression import getitem_expression, add_expression, mul_expression, neg_expression, div_expression, \
 rdiv_expression, pow_expression, rpow_expression
+from iqg import iqg
 
 from boundsurf import boundsurf
-try:
-    from boundsurf2 import boundsurf2
-except ImportError:
-    boundsurf2 = boundsurf
+from boundsurf2 import boundsurf2
     
 from FDmisc import FuncDesignerException, Diag, Eye, pWarn, scipyAbsentMsg, scipyInstalled, \
 raise_except, DiagonalType, isPyPy, formResolveSchedule, Len, Copy
 from ooPoint import ooPoint
 from FuncDesigner.multiarray import multiarray
 from derivativeMisc import getDerivativeSelf, considerSparse, mul_aux_d
-from Interval import Interval, adjust_lx_WithDiscreteDomain, adjust_ux_WithDiscreteDomain, mul_interval,\
+from Interval import Interval, mul_interval,\
 pow_const_interval, pow_oofun_interval, div_interval, add_interval, add_const_interval, \
 neg_interval, defaultIntervalEngine#, rdiv_interval
 #import inspect
 from baseClasses import OOArray, Stochastic, OOFun
-#from logic import AND, NOT, EQUIVALENT, nlh_not
 
 try:
     from DerApproximator import check_d1
@@ -355,155 +352,8 @@ class oofun(OOFun):
 
         return r            
     
-    def iqg(self, domain, dtype = float, lb=None, ub=None, UB = None):
-        if type(domain) != ooPoint:
-            domain = ooPoint(domain, skipArrayCast=True)
-            domain.isMultiPoint=True
-        domain.useSave = True
-        r0 = self.interval(domain, dtype, resetStoredIntervals = False)
-        
-        r0.lb, r0.ub = atleast_1d(r0.lb).copy(), atleast_1d(r0.ub).copy() # is copy required?
-        
-        # TODO: get rid of useSave
-        domain.useSave = False
-        
-        # TODO: rework it with indexation of required data
-        if lb is not None and ub is not None:
-            ind = logical_or(logical_or(r0.ub < lb, r0.lb > ub), all(logical_and(r0.lb >= lb, r0.ub <= ub)))
-        elif UB is not None:
-            ind = r0.lb > UB
-        else:
-            ind = None
-        
-        useSlicing = False
-        
-        if ind is not None:
-            if all(ind):
-                return {}, r0
-            j = where(~ind)[0]
-            #DOESN'T WORK FOR FIXED OOVARS AND DefiniteRange != TRUE YET
-            if 0 and j.size < 0.85*ind.size:  # at least 15% of values to skip
-                useSlicing = True
-                tmp = []
-                for key, val in domain.storedIntervals.items():
-                    Interval, definiteRange = val
-                    if type(definiteRange) not in (bool, bool_):
-                        definiteRange = definiteRange[j]
-                    tmp.append((key, (Interval[:, j], definiteRange)))
-                _storedIntervals = dict(tmp)
-                
-                Tmp = []
-                for key, val in domain.storedSums.items():
-                    # TODO: rework it
-                    R0, DefiniteRange0 = val.pop(-1)
-                    #R0, DefiniteRange0 = val[-1]
-                    R0 = R0[:, j]
-                    if type(DefiniteRange0) not in (bool, bool_):
-                        DefiniteRange0 = DefiniteRange0[j]
-                    tmp = []
-                    for k,v in val.items():
-                        # TODO: rework it
-#                        if k is (-1): continue
-                        v = v[:, j]
-                        tmp.append((k,v))
-                    val = dict(tmp)
-                    val[-1] = (R0, DefiniteRange0)
-                    Tmp.append((key,val))
-                _storedSums = dict(Tmp)
-                #domain.storedSums = dict(tmp)
-                
-                Tmp = []
-                for key, val in domain.items():
-                    lb_,ub_ = val
-                    # TODO: rework it when lb, ub will be implemented as 2-dimensional
-                    Tmp.append((key, (lb_[j],ub_[j])))
-                dictOfFixedFuncs = domain.dictOfFixedFuncs
-                domain2 = ooPoint(Tmp, skipArrayCast=True)
-                domain2.storedSums = _storedSums
-                domain2.storedIntervals = _storedIntervals
-                domain2.dictOfFixedFuncs = dictOfFixedFuncs
-                domain2.isMultiPoint=True
-                domain = domain2
-                
-        domain.useAsMutable = True
-        
-        r = {}
-        Dep = (self._getDep() if not self.is_oovar else set([self])).intersection(domain.keys())
-        
-        for i, v in enumerate(Dep):
-            domain.modificationVar = v
-            r_l, r_u = self._iqg(domain, dtype, r0)
-            if useSlicing and r_l is not r0:# r_l is r0 when array_equal(lb, ub)
-                lf1, lf2, uf1, uf2 = r_l.lb, r_u.lb, r_l.ub, r_u.ub
-                Lf1, Lf2, Uf1, Uf2 = Copy(r0.lb), Copy(r0.lb), Copy(r0.ub), Copy(r0.ub)
-                Lf1[:, j], Lf2[:, j], Uf1[:, j], Uf2[:, j] = lf1, lf2, uf1, uf2
-                r_l.lb, r_u.lb, r_l.ub, r_u.ub = Lf1, Lf2, Uf1, Uf2
-                if type(r0.definiteRange) not in (bool, bool_):
-                    d1, d2 = r_l.definiteRange, r_u.definiteRange
-                    D1, D2 = atleast_1d(r0.definiteRange).copy(), atleast_1d(r0.definiteRange).copy()
-                    D1[j], D2[j] = d1, d2
-                    r_l.definiteRange, r_u.definiteRange = D1, D2
-                
-            r[v] = r_l, r_u
-            if not self.isUncycled:
-                lf1, lf2, uf1, uf2 = r_l.lb, r_u.lb, r_l.ub, r_u.ub
-                lf, uf = nanmin(vstack((lf1, lf2)), 0), nanmax(vstack((uf1, uf2)), 0)
-                if i == 0:
-                    L, U = lf.copy(), uf.copy()
-                else:
-                    L[L<lf] = lf[L<lf].copy()
-                    U[U>uf] = uf[U>uf].copy()
-        if not self.isUncycled:
-            for R in r.values():
-                r1, r2 = R
-                if type(r1.lb) != ndarray:
-                    r1.lb, r2.lb, r1.ub, r2.ub = atleast_1d(r1.lb), atleast_1d(r2.lb), atleast_1d(r1.ub), atleast_1d(r2.ub)
-                r1.lb[r1.lb < L] = L[r1.lb < L]
-                r2.lb[r2.lb < L] = L[r2.lb < L]
-                r1.ub[r1.ub > U] = U[r1.ub > U]
-                r2.ub[r2.ub > U] = U[r2.ub > U]
-            
-            r0.lb[r0.lb < L] = L[r0.lb < L]
-            r0.ub[r0.ub > U] = U[r0.ub > U]
-            
-        # for more safety
-        domain.useSave = True
-        domain.useAsMutable = False
-        domain.modificationVar = None 
-        domain.storedIntervals = {}
-        
-        return r, r0
-    
-    def _iqg(self, domain, dtype, r0):
-        v = domain.modificationVar
-        v_0 = domain[v]
-        lb, ub = v_0[0], v_0[1]
+    iqg = iqg
 
-        if v.domain is not None and array_equal(lb, ub):
-            return r0,r0 
-
-        assert dtype in (float, float64, int32, int16),  'other types unimplemented yet'
-        middle = 0.5 * (lb+ub)
-        
-        if v.domain is not None:
-            middle1, middle2 = middle.copy(), middle.copy()
-            adjust_ux_WithDiscreteDomain(middle1, v)
-            adjust_lx_WithDiscreteDomain(middle2, v)
-        else:
-            middle1 = middle2 = middle
-        
-        domain[v] = (v_0[0], middle1)
-        domain.localStoredIntervals = {}
-        r_l = self.interval(domain, dtype, resetStoredIntervals = False)
-
-        domain[v] = (middle2, v_0[1])
-        domain.localStoredIntervals = {}
-        r_u = self.interval(domain, dtype, resetStoredIntervals = False)
-        
-        domain[v] = v_0
-        domain.localStoredIntervals = {}
-        return r_l, r_u
-            
     __pos__ = lambda self: self
 
     # overload "a+b"
