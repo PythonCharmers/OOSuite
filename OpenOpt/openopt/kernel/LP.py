b050527a54270ff13d310ee45e70ae4f2b81ed66
Ed Schofield <ed@pythoncharmers.com>
2014-10-03 15:17:09 +1000

    Fix another relative import

--- OpenOpt/openopt/kernel/LP.py
+++ OpenOpt/openopt/kernel/LP.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from future.builtins import range
-from .baseProblem import MatrixProblem
+from openopt.kernel.baseProblem import MatrixProblem
 from numpy import asarray, ones, inf, dot, zeros, asfarray, atleast_1d
 from . import NLP
 
